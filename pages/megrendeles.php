675e269e71215e7acf0dc0b9ba49a0aeda0c5c2e
Pistipisti94 <pistipisti57@gmail.com>
2023-11-07 10:07:45 +0100

    3.5

--- pages/megrendeles.php
+++ pages/megrendeles.php
@@ -3,21 +3,31 @@
 $userid = $_SESSION['user']['userid'];
 $termekid = filter_input(INPUT_GET, "termekid");
 $termek = $db->getKivalasztottTermek($termekid);
-if (filter_input(INPUT_POST, "rendeles", FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE)) {
+$feltetel = filter_input(INPUT_POST, "rendeles", FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+
+if ($feltetel) {
     $termekid = filter_input(INPUT_POST, "termekid", FILTER_VALIDATE_INT);
     $userid = $_SESSION['user']['userid'];
+    echo '<p>A rendelés megérkezett és feldolgozás alatt áll</p>';
     if ($db->setRendeles($termekid, $userid)) {
-        echo '<p>A rendelés megérkezett és feldolgozás alatt áll</p>';
+        
     } else {
         echo 'Sikertelen rögzítés';
     }
 }
-echo '<p>Valóban szeretné a(z) ' . $termek['marka'] . $termek['tipus']. ' termékünket megrendelni? </p>';
+
+
+
 //-- INSERT INTO `rendeles` (`termekid`, `userid`) VALUES ('3', '1');
-?>
-<form method="post" action="">
-    <input type="hidden" name="userid" value="<?php echo $_SESSION['user']['userid']; ?>">
-    <input type="hidden" name="termekid" value="<?php echo $termekid; ?>">
+if (!$feltetel) {
+    echo '<p>Valóban szeretné a(z) ' . $termek['marka'] . $termek['tipus'] . ' termékünket megrendelni? </p>';
+    echo '<form method="post" action="">
+    <input type="hidden" name="userid" value="' . $userid . '">
+    <input type="hidden" name="termekid" value="' . $termekid . '">
     <button type="submit" class="btn btn-success" name="rendeles" value="1">Igen</button>   
     <a href="index.php" class="btn btn-danger">Mégse</a>
-</form>
+</form>';
+} else {
+    
+}
+        
